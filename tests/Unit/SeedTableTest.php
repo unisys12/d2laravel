99fc73def8ad56e75df0a0de219cea1db9218136
Phillip Jackson <unisys12@gmail.com>
2020-12-08 22:06:04 -0600

    add test for checking for class and correct seeder called

--- tests/Unit/SeedTableTest.php
+++ tests/Unit/SeedTableTest.php
@@ -2,14 +2,22 @@
 
 namespace unisys12\D2Laravel\Tests\Unit;
 
-// use Illuminate\Support\Facades\Artisan;
 use unisys12\D2Laravel\Tests\TestCase;
 
 class SeedTableTest extends TestCase
 {
+
     /** @test */
     function the_SeedTable_class_should_be_loaded()
     {
-        $this->artisan('d2:seed manifest')->assertExitCode(0);
+        $this->assertTrue(class_exists(\unisys12\D2Laravel\Console\SeedTable::class));
+    }
+
+    /** @test */
+    function the_manifest_seeder_is_called_when_manifest_is_passed_to_seedtable()
+    {
+        $this->artisan('d2:seed', ["tables" => ["manifest"]])
+            ->expectsOutput('### Fetching Manifest ###')
+            ->assertExitCode(0);
     }
 }
